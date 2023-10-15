a8aca4872c6d8180502fae9700cc81909695b578
wejdanA <lol.lol.1417@hotmail.com>
2023-10-15 03:15:41 +0300

    React Advanced:Router

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <App />
+  <>
+    <BrowserRouter>
+      <Link to="/budget-app">Go to Budget App</Link>
+      <Routes>
+        <Route path="/budget-app" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </>
 );
